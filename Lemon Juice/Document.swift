97ed20eb15ce689d0e3370a578163755817824aa
Adam Cowdy <adam.cowdy@student.manchester.ac.uk>
2016-12-15 23:40:46 +0000

    Add smart link functionality and make the brace style more readable (Allman style)

--- Lemon Juice/Document.swift
+++ Lemon Juice/Document.swift
@@ -12,47 +12,55 @@ class Document: NSDocument {
     
     @IBOutlet weak var textView : NSTextView?
     
-    private var passwordKey = ""
+    private var passwordKey : String?
     private var textStorage : NSTextStorage?
 
-    override init() {
+    override init()
+    {
         super.init()
     }
 
-    override class func autosavesInPlace() -> Bool {
+    override class func autosavesInPlace() -> Bool
+    {
         return true
     }
     
     // Write files asynchronously
     class func canAsynchronouslyWrite(to url: URL, ofType typeName: String,
-                                      for saveOperation: NSSaveOperationType) -> Bool {
+                                      for saveOperation: NSSaveOperationType) -> Bool
+    {
         return true
     }
     
     // Read files asynchronously
-    override class func canConcurrentlyReadDocuments(ofType typeName: String) -> Bool {
+    override class func canConcurrentlyReadDocuments(ofType typeName: String) -> Bool
+    {
         return true
     }
     
-    override func windowControllerDidLoadNib(_ windowController: NSWindowController) {
-        // Use textStorage if it already exists e.g. a file has been opened,
+    override func windowControllerDidLoadNib(_ windowController: NSWindowController)
+    {
+        // Use the existing textStorage if it already exists e.g. a file has been opened,
         // else simply make the textStorage field a reference to textView.textStorage
-        if textStorage != nil {
+        if textStorage != nil
+        {
             textView!.layoutManager!.replaceTextStorage(textStorage!)
         }
-        else {
+        else
+        {
             textStorage = textView!.textStorage!
         }
     }
 
-    override var windowNibName: String? {
-        // Returns the nib file name of the document
+    // Returns the nib file name of the document
+    override var windowNibName: String?
+    {
         return "Document"
     }
     
     // Makes the data in the document available to save
-    override func data(ofType typeName: String) throws -> Data {
-        
+    override func data(ofType typeName: String) throws -> Data
+    {
         // Make the range the whole textStorage of textView
         let range = NSRange(location: 0, length: textView!.textStorage!.length)
         
@@ -65,12 +73,13 @@ class Document: NSDocument {
         return data
     }
 
-    override func read(from data: Data, ofType typeName: String) throws {
+    override func read(from data: Data, ofType typeName: String) throws
+    {
         var attributes = [NSDocumentTypeDocumentAttribute: NSRTFDTextDocumentType]
         let attributesPointer = AutoreleasingUnsafeMutablePointer<NSDictionary?>(&attributes)
         
         // Create a new text storage for the document
-        textStorage = NSTextStorage.init(rtfd: data, documentAttributes: attributesPointer)!
+        textStorage = NSTextStorage.init(rtfd: data, documentAttributes: attributesPointer)
     }
 }
 

54a80d3aa9d99d44f97d3eda78407f1dc5fe8d76
Adam Cowdy <cowdyadam@gmail.com>
2017-01-07 16:35:01 +0000

    Use the GNU GPL version 3

--- Lemon Juice/Encryption.swift
+++ Lemon Juice/Encryption.swift
@@ -2,9 +2,21 @@
 //  Encryption.swift
 //  Lemon Juice
 //
-//  Created by Adam Cowdy on 17/12/2016.
-//  Copyright © 2016 Adam Cowdy. All rights reserved.
 //
+//  Copyright (C) 2016, 2017  Adam Cowdy
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import Cocoa
 import Security
